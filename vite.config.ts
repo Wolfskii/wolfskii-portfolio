efa4c426b87ca409f78a221f1f5e27a6bdb15922
Dawid Kaleta <dawid.kaleta@outlook.com>
2023-05-11 17:12:32 +0200

    Add GitHub-actions CI/CD to push to GitHub-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'build'
+  },
   base: '/',
   plugins: [svelte()]
 })
