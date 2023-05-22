98c3d733196b550ce47bad54b66368ee94a9d70c
Dawid Kaleta <dawid.kaleta@outlook.com>
2023-05-22 20:58:36 +0200

    Start on footer layout

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,7 +1,19 @@
 <script lang="ts">
 </script>
 
-<footer />
+<footer>
+	<div class="left-footer">
+		<div class="footer-logo">A</div>
+
+		<div class="footer-socials">B</div>
+	</div>
+
+	<div class="right-footer">
+		<div class="footer-links">C</div>
+
+		<div class="footer-copyright">D</div>
+	</div>
+</footer>
 
 <style>
 </style>
